3948fd5e469b065f91d85c486fcf4f61e1aebde6
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2016-12-20 15:37:12 -0800

    added basic beginning setup for controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,29 @@
 class RestaurantsController < ApplicationController
+	def index
+		@restaurants = Restaurant.all
+	end
+
+	def new
+		@restaurant = Restaurant.new	
+	end
+
+	def create
+		@restaurant = current_user.restaurants.new(restaurant_params)
+		if @restaurant.save
+			redirect_to @restaurant
+		else
+			render "new"	
+		end
+	end
+
+	def show
+		@restaurant = Restaurant.find(params[:id])
+	end
+
+	private
+
+	def restaurant_params
+		params.require(:restaurant).permit(:name, :description, :avatar)
+	end
+
 end
