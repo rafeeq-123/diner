8e40df7dd4dcf41b30c7bad1b4f4ba268b939bff
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2016-12-21 11:01:13 -0800

    decided on 300x300 for thumb size

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -34,7 +34,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill=> [400, 400]
+    process :resize_to_fill=> [300, 300]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
