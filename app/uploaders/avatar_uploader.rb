eb241113e7ede7472fb510706e4340c25ff2f8ae
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2016-12-20 21:34:47 -0800

    fixed undefined method scale in uploader

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,7 +3,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -33,7 +33,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [50, 50]
+    process :resize_to_limit => [50, 50]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
