e28669948f1cbb88ab8a484792ad0b9a2e137e1d
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2016-12-25 06:37:02 -0800

    do not know why I had to add a desvie_scope to manually put in a get route to signout user, but ot works

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-	root "restaurants#index"
-  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
-  resources :restaurants
+	devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  get "welcome/index"
+  get "welcome/sponser"
+	root "welcome#index"
+  resources :restaurants do
+  	resources :votes
+  end
+  resources :resources
+  devise_scope :user do
+    get 'users/sign_out' => "devise/sessions#destroy"
+  end
 end
