dde55affe1e1c3f76c8560b6c8eded0fc0a2bd3d
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2016-12-27 10:36:22 -0800

    fixing controller and adding ajax for better user UI

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,27 @@
+class VotesController < ApplicationController
+	
+	def new
+		@vote = Vote.new
+	end
+
+	def create
+		@restaurant = Restaurant.find(params[:restaurant_id])
+    @vote = @restaurant.votes.new(vote_params)
+    @vote.user_id = current_user.id
+    respond_to do |format|
+    	if @vote.save
+    		format.html { redirect_to @restaurant, notice: "Thank you for your support!"}
+    		format.js {}
+    		format.json {render json: @restaurant, status: :created, location: @restaurant}
+    	else
+    		format.html { render action: "show" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }	
+      end
+    end
+	end
+
+	private
+	def vote_params
+		params.require(:vote).permit(:like)
+	end
+end
