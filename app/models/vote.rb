a66dd69283ef3a859f3325592fc376a0c7209fe6
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2016-12-27 16:17:15 -0800

    added vaildation to keep users from making more than one like on a given object

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,6 @@
 class Vote < ApplicationRecord
 	belongs_to :user
 	belongs_to :restaurant
+	validates_uniqueness_of :restaurant_id, scope: :user_id
+	# validates :likes, uniqueness: { scope: :user_id}
 end
