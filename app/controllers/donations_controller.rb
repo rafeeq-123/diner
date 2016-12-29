0dde0c405c22142f50173cbd1dda12f71a2b500c
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2016-12-28 21:18:52 -0800

    fixed spacing

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -8,22 +8,21 @@ class DonationsController < ApplicationController
 	end
 
 	def create
-	  @restaurant = Restaurant.find(params[:restaurant_id])
-    @donation = @restaurant.donations.new(donation_params)
-    @donation.user_id = current_user.id
-    @donation.amount - 5
-    	respond_to do |format|
-    	if @donation.save
-    	  format.html { redirect_to @restaurant, notice: "Thank you for giving and for supporting the community and donating!"}
-    	  format.js {}
-    	  format.json {render json: @restaurant, status: :created, location: @restaurant}
-    	else
-    	  format.html { redirect_to @restaurant, notice: "The associated account with email #{current_user.email} now has #{@donation.amount - 5 } cerdits to give"}
-    	  format.js {}
-        format.json { render json: @restaurant, notice: "Users are only able to donate once"}
-        end  				
-      end
-	  end
+		@restaurant = Restaurant.find(params[:restaurant_id])
+		@donation = @restaurant.donations.new(donation_params)
+		@donation.user_id = current_user.id
+			respond_to do |format|
+			if @donation.save
+				format.html { redirect_to @restaurant, notice: "Thank you for giving and for supporting the community and donating!"}
+				format.js {}
+				format.json {render json: @restaurant, status: :created, location: @restaurant}
+			else
+				format.html { redirect_to @restaurant, notice: "The associated account with email #{current_user.email} now has #{@donation.amount - 5 } cerdits to give"}
+				format.js {}
+				format.json { render json: @restaurant, notice: "Users are only able to donate once"}
+				end  				
+			end
+		end
 
 	private
 	def donation_params
