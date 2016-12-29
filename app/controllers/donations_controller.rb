1ecf4100c028c16251bc68e72b1fff38d614c7a0
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2016-12-29 11:20:16 -0800

    fixed problem with negative accounts in notice message

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -18,7 +18,7 @@ class DonationsController < ApplicationController
         format.js {}
         format.json {render json: @restaurant, status: :created, location: @restaurant}
       else
-        format.html { redirect_to @restaurant, notice: "The associated account with email #{current_user.email} now has #{@donation.amount - 5 } cerdits to give"}
+        format.html { redirect_to @restaurant, notice: "The associated account with email #{current_user.email}  has  #{@donation.amount} cerdits to give"}
         format.js {}
         format.json { render json: @restaurant, notice: "Users are only able to donate once"}
         end         
