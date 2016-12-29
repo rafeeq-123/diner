23d4da49a3f8a53b782c938ca08164d4e1c181a5
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2016-12-28 20:03:17 -0800

    adding event time and ticket vaildiations to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,14 @@
 class Restaurant < ApplicationRecord
 validates :name, presence: true
 validates :description, presence: true	
-validates :avatar, presence: true	
+validates :event_time, presence: true	
+validates :availability, presence: true	
+validates :avatar, presence:  { message: "You will need to pick a logo for your restaurant" }	
 belongs_to :user
 geocoded_by :address 
 after_validation :geocode
 mount_uploader :avatar, AvatarUploader
 has_many :votes, through: :users
 has_many :votes
+has_many :donations
 end
