d2db551cb4d3ca3c978c736301e848ed2a6d075b
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2016-12-29 13:54:07 -0800

    adding d3.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require turbolinks
+//= require d3
 //= require_tree 
 
                                         
--- app/assets/javascripts/restaurants.js
+++ app/assets/javascripts/restaurants.js
@@ -3,4 +3,4 @@ function blinker() {
     $('.blink_me').fadeIn(500);
 }
 
-setInterval(blinker, 1000);
+setInterval(blinker, 1500);
