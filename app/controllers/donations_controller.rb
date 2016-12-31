d374a51977eb9b1a1354b4d829b36339914112cf
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2016-12-31 13:59:59 -0800

    added the current user information in credit action

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,57 +1,40 @@
 class DonationsController < ApplicationController
 
-  def index
-  end
+	def index
+	end
 
-  def credit
-  end
+	def new
+		@donation = Donation.new
+	end			
 
-  def add_credit
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @donation = @restaurant.donations.new(donation_params)
-    @donation.user_id = current_user.id
-    @donation.increment(:amount, by = 5)
-      respond_to do |format|
-      if @donation.save
-        format.html { redirect_to @restaurant, notice: "Your account now have #{@donation.amount} "}
-        format.js {}
-        format.json {render json: @restaurant, status: :created, location: @restaurant}
-      else
-        format.html { redirect_to @restaurant, notice: "The associated account with email #{current_user.email} was not to recieve credit"}
-        format.js {}
-        format.json { render json: @restaurant, notice: "Users are only able to donate once"}
-        end         
-      end
-  end
+	def credit
+		@donation = Donation.where(user_id: current_user.id)
+	end
 
-  def new
-    @donation = Donation.new
-  end
+	def create
+		@restaurant = Restaurant.find(params[:restaurant_id])
+		@donation = @restaurant.donations.new(donation_params)
+		@donation.user_id = current_user.id
+		@donation.decrement(:amount, by = 5)
+			respond_to do |format|
+			if @donation.save
+				format.html { redirect_to @restaurant, notice: "Thank you for giving and for supporting the community and donating!"}
+				format.js {}
+				format.json {render json: @restaurant, status: :created, location: @restaurant}
+			else
+				format.html { redirect_to @restaurant, notice: "The associated account with email #{current_user.email}  has  #{@donation.amount} cerdits to give"}
+				format.js {}
+				format.json { render json: @restaurant, notice: "Users are only able to donate once"}
+				end         
+			end
+		end
 
-  def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @donation = @restaurant.donations.new(donation_params)
-    @donation.user_id = current_user.id
-    @donation.decrement(:amount, by = 5)
-      respond_to do |format|
-      if @donation.save
-        format.html { redirect_to @restaurant, notice: "Thank you for giving and for supporting the community and donating!"}
-        format.js {}
-        format.json {render json: @restaurant, status: :created, location: @restaurant}
-      else
-        format.html { redirect_to @restaurant, notice: "The associated account with email #{current_user.email}  has  #{@donation.amount} cerdits to give"}
-        format.js {}
-        format.json { render json: @restaurant, notice: "Users are only able to donate once"}
-        end         
-      end
-    end
+		def show
+			@donation = Donation.where(user_id: current_user.id)
+		end
 
-    def show
-      @donation = Donation.where(user_id: current_user.id)
-    end
-
-  private
-  def donation_params
-    params.require(:donation).permit(:amount)
-  end
+	private
+	def donation_params
+		params.require(:donation).permit(:amount)
+	end
 end
