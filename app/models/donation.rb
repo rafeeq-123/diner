116c6bb1977f3afeec2a2d2df683b0ea775b1984
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2017-01-01 18:15:12 -0800

    edited too long method to multiline

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,5 +1,28 @@
 class Donation < ApplicationRecord
 	belongs_to :user
 	belongs_to :restaurant
-	validates_uniqueness_of :user_id
+	validates_uniqueness_of :user_id, :unless => :ready_for_credit? 
+	validates :amount, numericality: { greater_than_or_equal_to: 0 }
+
+	def ready_for_credit?
+		#only ranging max  count of instances
+		don_countable = user.donations.count
+		user.donations.collect(&:amount)[0..don_countable].inject(0){|sum,x| sum + x } == 0 && 
+		user_vote && user_restaurant && user_donation
+	end
+
+	def user_vote
+		vote_count = user.votes.count
+		vote_count == [5..8]|| vote_count == [8..15]
+	end
+
+	def user_restaurant
+		restaurant_count = user.restaurants.count 
+		restaurant_count == [5..10] || restaurant_count == [20..25]
+	end
+
+	def user_donation
+		donation_count = user.donations.count 
+		donation_count == 1 || donation_count ==  2	
+	end
 end
