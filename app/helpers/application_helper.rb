ffa0bdc480c3afc7e189c10cec2de678753db29f
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2017-01-01 18:07:55 -0800

    added user has stats to be displayed when user is active and ready for credit method to also be displayed when user reaches enough usage to warrent mre credit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,16 +5,33 @@ module ApplicationHelper
 	end
 
 	def ready_for_credit?
-		#change votes to 4 after adding funtionality
-		user_signed_in? && donation_count == 0 && votes >= 4 && current_user.restaurants.count >= 2
+		#only ranging max  count of instances
+		current_user_count = current_user.donations.count
+		user_signed_in? && current_user.donations.collect(&:amount)[0..current_user_count].inject(0){|sum,x| sum + x } == 0 && user_vote && user_restaurant && user_donation
 	end
 
 	def votes
-		array = current_user.votes.collect(&:likes)
-		array.inject(0){|sum,x| sum + x }
+		vote_array = current_user.votes.collect(&:likes)
+		vote_array.inject(0){|sum,x| sum + x }
 	end
 
 	def donation_count
-		current_user.donations.collect(&:amount)[0]
+		#only ranging max  count of instances
+		don_countable = @restaurant.donations.count
+		@restaurant.donations.collect(&:amount)[0..don_countable].inject(0){|sum,x| sum + x }
+	end
+	def user_vote
+		vote_count = current_user.votes.count
+		vote_count == [5..8]|| vote_count == [8..15]
+	end
+
+	def user_restaurant
+		restaurant_count = current_user.restaurants.count 
+		restaurant_count == [5..10] || restaurant_count == [20..25]
+	end
+
+	def user_donation
+		donation_count = current_user.donations.count 
+		donation_count == 1 || donation_count ==  2	
 	end
 end
