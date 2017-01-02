8aa2937b730cadeac44442eeba5fcd25aa1a6d9d
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2017-01-01 18:05:38 -0800

    removed nested create action for bonus credit edge case to put in model as a custom vaildation

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -3,8 +3,8 @@ class DonationsController < ApplicationController
 	def index
 	end
 
-	def new
-		@donation = Donation.new
+	def new		
+  	# @donation = Donation.new
 	end			
 
 	def credit
@@ -12,29 +12,43 @@ class DonationsController < ApplicationController
 	end
 
 	def create
+		# binding.pry
 		@restaurant = Restaurant.find(params[:restaurant_id])
 		@donation = @restaurant.donations.new(donation_params)
-		@donation.user_id = current_user.id
-		@donation.decrement(:amount, by = 5)
-			respond_to do |format|
+	  @donation.user_id = current_user.id
+	  @donation.amount -= 5
+		respond_to do |format|
 			if @donation.save
-				format.html { redirect_to @restaurant, notice: "Thank you for giving and for supporting the community and donating!"}
+				format.html { redirect_to @restaurant, notice: "Thank you for giving and for supporting the community and donating! Your account now has the balance: #{amount_sum}"}
 				format.js {}
 				format.json {render json: @restaurant, status: :created, location: @restaurant}
-			else
-				format.html { redirect_to @restaurant, notice: "The associated account with email #{current_user.email}  has  #{@donation.amount} cerdits to give"}
+		  else
+				format.html { redirect_to @restaurant, notice: "The associated account with email #{current_user.email} and account balance #{amount_sum}"}
 				format.js {}
 				format.json { render json: @restaurant, notice: "Users are only able to donate once"}
-				end         
 			end
 		end
-
-		def show
-			@donation = Donation.where(user_id: current_user.id)
-		end
+	end
 
 	private
+
 	def donation_params
 		params.require(:donation).permit(:amount)
+	end
+
+	def amount_sum
+		# binding.pry
+		# cleaned_num = ""
+		# if current_user.donations.count >= 2 
+			#something is happening here and its not allowing the default of 5 to work for new user
+			# could it be the postion of this method? what is going ?
+			# take out this method to diagnose
+		#need to find a better why to check how many instance there are other then a guess range
+		 @restaurant.donations.collect(&:amount)[0..50].inject(0){|sum,x| sum + x }
+
+		  # if current_user.id.nil? && @donation != nil
+		  #  @donation.user_id = current_user.id
+    #   @donation.amount.to_i
+    # end
 	end
 end
