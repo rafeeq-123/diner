257e2e8a49e7a69c4e6bc10440d1f26b743793ef
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2017-01-11 16:43:48 -0800

    fixing spaces

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,16 +1,16 @@
 class DonationsController < ApplicationController
-
+	
 	def index 
 	end
 
 	def new		
-  	@donation = Donation.new
+		@donation = Donation.new
 	end			
 
 	def credit
 		@donation = Donation.where(user_id: current_user.id)
 	end
-	
+
 	def create
 		@restaurant = Restaurant.find(params[:restaurant_id])
 		@donation = @restaurant.donations.new(donation_params)
@@ -21,7 +21,7 @@ class DonationsController < ApplicationController
 				format.html { redirect_to @restaurant, notice: "Thank you for giving and for supporting the community and donating! Your account now has the balance: #{amount_sum}"}
 				format.js {}
 				format.json {render json: @restaurant, status: :created, location: @restaurant}
-		  else
+			else
 				format.html { redirect_to @restaurant, notice: "Sorry, the associated account with email: #{current_user.email} has an account balance of #{amount_sum}"}
 				format.js {}
 				format.json { render json: @restaurant, notice: "Users are only able to donate once"}
@@ -39,15 +39,15 @@ class DonationsController < ApplicationController
 		# binding.pry
 		# cleaned_num = ""
 		# if current_user.donations.count >= 2 
-			#something is happening here and its not allowing the default of 5 to work for new user
-			# could it be the postion of this method? what is going ?
-			# take out this method to diagnose
+		#something is happening here and its not allowing the default of 5 to work for new user
+		# could it be the postion of this method? what is going ?
+		# take out this method to diagnose
 		#need to find a better why to check how many instance there are other then a guess range
-		 @restaurant.donations.collect(&:amount)[0..50].inject(0){|sum,x| sum + x }
-
-		  # if current_user.id.nil? && @donation != nil
-		  #  @donation.user_id = current_user.id
-    #   @donation.amount.to_i
-    # end
+		@restaurant.donations.collect(&:amount)[0..50].inject(0){|sum,x| sum + x }
+	
+		# if current_user.id.nil? && @donation != nil
+		#  @donation.user_id = current_user.id
+		#   @donation.amount.to_i
+		# end
 	end
 end
